40ff650310802c9bf87614b29a353533bb0dc44f
Leonardo <leu1.g.carvalho@gmail.com>
2024-09-10 20:14:55 -0300

    feat(NavBar): navbar added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
-import { CiShop, CiShoppingBasket, CiMapPin   } from "react-icons/ci";
+import { CiShop, CiShoppingBasket, CiMapPin } from "react-icons/ci";
+import { BsArrowRight } from "react-icons/bs";
 import styles from "./Navbar.module.css";
 
 export function Navbar() {
@@ -7,9 +8,11 @@ export function Navbar() {
       <button className={styles.shop}>
         <CiShop />
       </button>
+      <BsArrowRight />
       <button className={styles.products}>
         <CiShoppingBasket />
       </button>
+      <BsArrowRight />
       <button className={styles.changes}>
         <CiMapPin />
       </button>
