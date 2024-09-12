9ef8a6e4940d5461aaf0a3bb229b8c2072ef7742
Leonardo <leu1.g.carvalho@gmail.com>
2024-09-12 01:10:06 -0300

    feat(products): products component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
-import { Header } from './components/Header';
-import { Navbar } from './components/Navbar';
-import { Markets } from './components/Markets';
-import { Footer } from './components/Footer';
-import styles from './App.module.css';
+import { Header } from "./components/Header";
+// import { Navbar } from "./components/Navbar";
+// import { Markets } from './components/Markets';
+import { Products } from "./components/Products";
+import { Footer } from "./components/Footer";
+import styles from "./App.module.css";
+import { CiShop, CiShoppingBasket, CiMapPin } from "react-icons/ci";
+import { BsArrowRight } from "react-icons/bs";
 
 export function App() {
+
   return (
     <div>
       <Header />
-
+{/* --------------------------------------------------------- */}
       <div className={styles.bodyApp}>
-        <Navbar />
-
-        <div className={styles.content}>
-          <Markets />
-          <Markets />
-          <Markets />
+        <div className={styles.container}>
+          <button className={styles.shop}>
+            <CiShop />
+          </button>
+          <BsArrowRight />
+          <button className={styles.products}>
+            <CiShoppingBasket />
+          </button>
+          <BsArrowRight />
+          <button className={styles.changes}>
+            <CiMapPin />
+          </button>
         </div>
-        
+{/* --------------------------------------------------------- */}
+        <div className={styles.content}>
+          <Products />
+        </div>
       </div>
-
+{/* --------------------------------------------------------- */}
       <Footer />
     </div>
-  )
+  );
 }
