cc4c85785c98cbb3687cd5893a4094cfda4cefb8
Leonardo <leu1.g.carvalho@gmail.com>
2024-09-11 22:25:20 -0300

    fix(footer): style changes on footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,17 +5,37 @@ import {
   FaLaptopCode,
 } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { LuMail, LuMailOpen } from "react-icons/lu";
+// import { LuMail, LuMailOpen } from "react-icons/lu";
 
 export function Footer() {
   return (
     <footer className={styles.footerArea}>
-      <div className={styles.contact}>
-        <h1>Get in Touch</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          itaque facere reiciendis accusantium.
-        </p>
+      <div className={styles.links}>
+        <div className={styles.contact}>
+          <h1>Get in Touch</h1>
+          <p>About Us</p>
+          <p>Our Services</p>
+          <p>Privacy Policy</p>
+          <p>FAQ</p>
+        </div>
+        <div className={styles.contact}>
+          <h1>Get in Touch</h1>
+          <p>About Us</p>
+          <p>Our Services</p>
+          <p>Privacy Policy</p>
+          <p>FAQ</p>
+        </div>
+        <div className={styles.contact}>
+          <h1>Get in Touch</h1>
+          <p>About Us</p>
+          <p>Our Services</p>
+          <p>Privacy Policy</p>
+          <p>FAQ</p>
+        </div>
+      </div>
+
+      <div className={styles.iconsArea}>
+        <h1>Follow Us</h1>
         <div className={styles.icons}>
           <FaWhatsappSquare />
           <FaInstagramSquare />
@@ -24,7 +44,7 @@ export function Footer() {
       </div>
 
       <div className={styles.card1}>
-        <FaLaptopCode className={styles.card1Img}/> L & G
+        <FaLaptopCode className={styles.card1Img} /> L & G
       </div>
     </footer>
   );
