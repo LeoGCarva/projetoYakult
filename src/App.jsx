3af0b4786a4fb3d4cae07ff259a0b83c7578ac62
Leonardo <leu1.g.carvalho@gmail.com>
2024-09-19 15:53:56 -0300

    footer fixed

--- src/App.jsx
+++ src/App.jsx
@@ -107,116 +107,115 @@ export function App() {
   }
 
   return (
-    <div className={styles.wrapperBodyApp}>
-      <Header />
-      {/* --NavBar------------------------------------------------ */}
-      <div className={styles.bodyApp}>
-        <div className={styles.container}>
-          <button className={styles.shop} onClick={() => setStep(0)}>
-            <CiShop />
-          </button>
-          <BsArrowRight />
-          <button
-            className={styles.products}
-            onClick={() => setStep(1)}
-            disabled={!watch("storeName")}
-          >
-            <CiShoppingBasket />
-          </button>
-          <BsArrowRight />
-          <button
-            className={styles.changes}
-            onClick={() => setStep(2)}
-            disabled={step < 2}
-          >
-            <CiMapPin />
-          </button>
-        </div>
-        {/* --------------------------------------------------------- */}
-        {step === 0 ? (
-          <div className={styles.content}>
-            {stores.map((store) => (
-              <Markets
-                handleStoreName={handleStoreName}
-                key={store.id}
-                id={store.id}
-                name={store.name}
-                local={store.local}
-                settedStoreName={watch("storeName")}
-              />
-            ))}
-          </div>
-        ) : null}
-        {step === 1 ? (
-          <div className={styles.content}>
-            <button
-              onClick={handleSubmit(finishOrder)}
-              className={styles.btnFinishOrder}
-            >
-              Finalizar
+    <>
+        <Header />
+        {/* --NavBar------------------------------------------------ */}
+        <div className={styles.bodyApp}>
+          <div className={styles.container}>
+            <button className={styles.shop} onClick={() => setStep(0)}>
+              <CiShop />
             </button>
-            {fieldsProducts.map((field, index) => (
-              <div key={field.id} className={styles.productContainer}>
-                <div className={styles.productImg}>
-                  <img src={watch(`products.${index}.img`)} alt="" />
-                  <div className={styles.productInfo}>
-                    <p className={styles.name}>
-                      {watch(`products.${index}.name`)}
-                    </p>
-                    <p className={styles.price}>
-                      Preço da unidade: R${watch(`products.${index}.price`)}
-                    </p>
-                    <p className={styles.amount}>
-                      Quantidade:
-                      <input
-                        onChange={(event) => {
-                          if (!/[0-9.]*/.test(event.key)) {
-                            event.preventDefault();
-                          }
-                        }}
-                        type="number"
-                        min="0"
-                        step="1"
-                        {...register(`products.${index}.amount`)}
-                        className={styles.input}
-                      />
-                    </p>
-                  </div>
-                </div>
-                <div className={styles.productContent}></div>
-              </div>
-            ))}
+            <BsArrowRight />
+            <button
+              className={styles.products}
+              onClick={() => setStep(1)}
+              disabled={!watch("storeName")}
+            >
+              <CiShoppingBasket />
+            </button>
+            <BsArrowRight />
+            <button
+              className={styles.changes}
+              onClick={() => setStep(2)}
+              disabled={step < 2}
+            >
+              <CiMapPin />
+            </button>
           </div>
-        ) : null}
-
-        {step === 2 ? (
-          <div className={styles.content}>
-            <div className={styles.wrapperOrder}>
-              <h2>Pedidos</h2>
-              <p className={styles.descAmount}>Quantidade</p>
-              <p className={styles.descName}>Nome</p>
-              <p className={styles.descPrice}>Preço</p>
-              <p className={styles.descTotal}>Total</p>
-
-              <div className={styles.wrapperOrderReceipt}>
-                {orders.map((order) => (
-                  <div key={order.id} className={styles.orderReceipt}>
-                    <p className={styles.orderAmount}>{order.amount}</p>
-                    <p className={styles.orderName}>{order.name}</p>
-                    <p className={styles.orderPrice}>R$ {order.price}</p>
-                    <p className={styles.orderTotalAmount}>
-                      R$ {order.totalAmount}
-                    </p>
+          {/* --------------------------------------------------------- */}
+          {step === 0 ? (
+            <div className={styles.content}>
+              {stores.map((store) => (
+                <Markets
+                  handleStoreName={handleStoreName}
+                  key={store.id}
+                  id={store.id}
+                  name={store.name}
+                  local={store.local}
+                  settedStoreName={watch("storeName")}
+                />
+              ))}
+            </div>
+          ) : null}
+          {step === 1 ? (
+            <div className={styles.content}>
+              <button
+                onClick={handleSubmit(finishOrder)}
+                className={styles.btnFinishOrder}
+              >
+                Finalizar
+              </button>
+              {fieldsProducts.map((field, index) => (
+                <div key={field.id} className={styles.productContainer}>
+                  <div className={styles.productImg}>
+                    <img src={watch(`products.${index}.img`)} alt="" />
+                    <div className={styles.productInfo}>
+                      <p className={styles.name}>
+                        {watch(`products.${index}.name`)}
+                      </p>
+                      <p className={styles.price}>
+                        Preço da unidade: R${watch(`products.${index}.price`)}
+                      </p>
+                      <p className={styles.amount}>
+                        Quantidade:
+                        <input
+                          onChange={(event) => {
+                            if (!/[0-9.]*/.test(event.key)) {
+                              event.preventDefault();
+                            }
+                          }}
+                          type="number"
+                          min="0"
+                          step="1"
+                          {...register(`products.${index}.amount`)}
+                          className={styles.input}
+                        />
+                      </p>
+                    </div>
                   </div>
-                ))}
+                  <div className={styles.productContent}></div>
+                </div>
+              ))}
+            </div>
+          ) : null}
+
+          {step === 2 ? (
+            <div className={styles.content}>
+              <div className={styles.wrapperOrder}>
+                <h2>Pedidos</h2>
+                <p className={styles.descAmount}>Quantidade</p>
+                <p className={styles.descName}>Nome</p>
+                <p className={styles.descPrice}>Preço</p>
+                <p className={styles.descTotal}>Total</p>
+
+                <div className={styles.wrapperOrderReceipt}>
+                  {orders.map((order) => (
+                    <div key={order.id} className={styles.orderReceipt}>
+                      <p className={styles.orderAmount}>{order.amount}</p>
+                      <p className={styles.orderName}>{order.name}</p>
+                      <p className={styles.orderPrice}>R$ {order.price}</p>
+                      <p className={styles.orderTotalAmount}>
+                        R$ {order.totalAmount}
+                      </p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ) : null}
-      </div>
-      {/* --------------------------------------------------------- */}
-
+          ) : null}
+        </div>
+        {/* --------------------------------------------------------- */}
       <Footer />
-    </div>
+    </>
   );
 }
